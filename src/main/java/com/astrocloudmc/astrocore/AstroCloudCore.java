82783a93a3d96676db047465d10f97609853f6e6
ThirstdayTime <thirstdaytime@gmail.com>
2025-03-02 14:52:02 +0800

    Initial commit

---
+++ src/main/java/com/astrocloudmc/astrocore/AstroCloudCore.java
@@ -0,0 +1,87 @@
+package com.astrocloudmc.astrocore;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+/**
+ * PluginTemplate is a simple template you can use every time you make
+ * a new plugin. This will save you time because you no longer have to
+ * recreate the same skeleton and features each time.
+ *
+ * It uses Foundation for fast and efficient development process.
+ */
+public final class AstroCloudCore extends SimplePlugin {
+
+	/**
+	* Automatically perform login ONCE when the plugin starts.
+	*/
+	@Override
+	protected void onPluginStart() {
+
+		String dead = "testicles";
+
+		dead = "hi";
+	}
+
+	/**
+	 * Automatically perform login when the plugin starts and each time it is reloaded.
+	 */
+	@Override
+	protected void onReloadablesStart() {
+
+		// You can check for necessary plugins and disable loading if they are missing
+		//Valid.checkBoolean(HookManager.isVaultLoaded(), "You need to install Vault so that we can work with packets, offline player data, prefixes and groups.");
+
+		// Uncomment to load variables
+		// Variable.loadVariables();
+
+		//
+		// Add your own plugin parts to load automatically here
+		// Please see @AutoRegister for parts you do not have to register manually
+		//
+	}
+
+	@Override
+	protected void onPluginPreReload() {
+
+
+
+		// Close your database here if you use one
+		//YourDatabase.getInstance().close();
+	}
+
+	/* ------------------------------------------------------------------------------- */
+	/* Events */
+	/* ------------------------------------------------------------------------------- */
+
+	/**
+	 * An example event that checks if the right clicked entity is a cow, and makes an explosion.
+	 * You can write your events to your main class without having to register a listener.
+	 *
+	 * @param event
+	 */
+	@EventHandler
+	public void onRightClick(final PlayerInteractEntityEvent event) {
+
+	event.getRightClicked().setFireTicks(100);
+
+	//	if (event.getRightClicked().getType() == EntityType.ZOMBIE)
+	//		event.getRightClicked().getWorld().createExplosion(event.getRightClicked().getLocation(), 5);
+	}
+
+	/* ------------------------------------------------------------------------------- */
+	/* Static */
+	/* ------------------------------------------------------------------------------- */
+
+	/**
+	 * Return the instance of this plugin, which simply refers to a static
+	 * field already created for you in SimplePlugin but casts it to your
+	 * specific plugin instance for your convenience.
+	 *
+	 * @return
+	 */
+	public static AstroCloudCore getInstance() {
+		return (AstroCloudCore) SimplePlugin.getInstance();
+	}
+}
